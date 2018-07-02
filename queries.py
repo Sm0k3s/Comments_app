b7f6237da0db5b06cff80c8fc7c72e45ef10c3df
fgkinus <fgkinus@gmail.com>
2018-07-02 14:12:13 +0300

    added comment modification code

--- queries.py
+++ queries.py
@@ -2,10 +2,11 @@ add_user = """insert into users(username,password) values (%s,%s);"""
 add_previledge = """
 insert into previledges(user_id, previledge) values (%s,%s)"""
 add_comment = """insert into comments(author, text, parent) values (%s,%s,%s)"""
+check_previledge = """select previledge from previledges where user_id = %s;"""
 
 get_user = """select * from users where  username = %s;"""
 get_comment = """select * from comments where id = %s;"""
 get_children = """select * from comments where parent =%s"""
 
-
-edit_comment = """"""
+edit_comment = """update comments set (text,updated ) = (%s,%s);"""
+delete_comment = """delete from comments where id = %s;"""
